b1b219e2b37d566f3b0f89e3982e2bbed69a3840
David Miles <davidmiles1925@gmail.com>
2023-11-24 00:16:55 -0600

    tweak stop_program

--- stop_program.py
+++ stop_program.py
@@ -30,11 +30,12 @@ def stop_motion_program():
             for line in lines:
                 if f'{filename}' in line:
                     # Extract the PID from the output
-                    pid = int(line.split()[1])
+                    pid = int(line.split()[1]) 
 
                     # Stop the process using the PID
                     os.system(f"sudo kill -TERM {pid}")
                     print(f"Process with PID {pid}  has been stopped.")
+                    console_and_log(f"The program was stopped via SSH connection. Process ('{filename}') with PID {pid}")
                     return True  # Exit the function after stopping the process
             print(f"No process with '{filename}' found.")
             return False  # No process found to stop
@@ -55,4 +56,3 @@ def console_and_log(message=""):
         write_to_log(message)
 
 stop_motion_program()
-console_and_log(f"The program was stopped via SSH connection. Process ('{filename}') with PID {pid}") 
